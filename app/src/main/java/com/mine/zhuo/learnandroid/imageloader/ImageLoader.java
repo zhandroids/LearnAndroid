dd3efb4aa53b2251037fbd8efc047fa0a8cbc2c7
zhangzhuo <862340288@qq.com>
2018-12-12 19:00:58 +0800

    增加的图片缓存

--- app/src/main/java/com/mine/zhuo/learnandroid/imageloader/ImageLoader.java
+++ app/src/main/java/com/mine/zhuo/learnandroid/imageloader/ImageLoader.java
@@ -1,10 +1,153 @@
 package com.mine.zhuo.learnandroid.imageloader;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import com.mine.zhuo.learnandroid.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by Administrator on 2018/12/11.
  */
 
 public class ImageLoader {
 
-//    private static final
+    private static final String TAG = "ImageLoader";
+
+    final int MESSAGE_POST_RESULT = 1;
+
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final int CORE_POOL_SIZE = CPU_COUNT + 1;
+    private static final int MAX_POOL_SIZE = CORE_POOL_SIZE * 2 + 1;
+    private static final long KEEP_ALIVE = 10L;
+    private static final int TAG_KEY_URI = R.id.all;
+    private static final long DISK_CACHE_SIZE = 1024 * 1024 * 50;
+    public static final int IO_BUFFER_SIZE = 1024 * 8;
+    private static final int DISK_CACHE_INDEX = 0;
+    private boolean mIsDiskLruCacheCreated = false;
+
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+        private final AtomicInteger mCount = new AtomicInteger();
+
+        @Override
+        public Thread newThread(@NonNull Runnable r) {
+            return new Thread(r, "ImageLoader#" + mCount.getAndIncrement());
+        }
+    };
+
+    public static final Executor THREAD_POOL_EXCUTOR = new ThreadPoolExecutor(CORE_POOL_SIZE,
+            MAX_POOL_SIZE, KEEP_ALIVE,
+            TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), sThreadFactory);
+
+
+    private Handler mMainHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            LoaderResult loaderResult = (LoaderResult) msg.obj;
+            ImageView imageView = loaderResult.imageView;
+            String uri = (String) imageView.getTag();
+
+            if (uri.equals(loaderResult.url)) {
+                imageView.setImageBitmap(loaderResult.bitmap);
+            } else {
+                Log.w(TAG, "set image bitmap,but url has changed, ignored!");
+            }
+        }
+    };
+
+    private Context mContext;
+    private ImageResizer imageResizer = new ImageResizer();
+    private LruCache<String, Bitmap> mMemoryCache;
+    private DiskLruCache mDiskCache;
+
+    private ImageLoader(Context context) {
+        mContext = context.getApplicationContext();
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        int cacheSize = maxMemory / 8;
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getRowBytes() * value.getHeight() / 1024;
+            }
+        };
+
+        File diskCacheDir = getDiskCacheDir(context,"bitmap");
+        if (!diskCacheDir.exists()){
+            diskCacheDir.mkdir();
+        }
+
+        if (getUsableSpace(diskCacheDir)>DISK_CACHE_SIZE){
+            try {
+                mDiskCache = DiskLruCache.open(diskCacheDir,1,1,DISK_CACHE_SIZE);
+
+                mIsDiskLruCacheCreated = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static ImageLoader build(Context context){
+        return new ImageLoader(context);
+    }
+
+    private  void addBitmapToMemoryCache(String key,Bitmap bitmap){
+        if (getBitmapFromMemCache(key)==null){
+            mMemoryCache.put(key,bitmap);
+        }
+    }
+
+    private Bitmap getBitmapFromMemCache(String key){
+        return mMemoryCache.get(key);
+    }
+
+
+
+    public File getDiskCacheDir(Context context, String uniqueName) {
+        boolean externalStorageAvailable = Environment.getExternalStorageState().equals
+                (Environment.MEDIA_MOUNTED);
+
+        final String cachePath;
+        if (externalStorageAvailable){
+            cachePath = context.getExternalCacheDir().getPath();
+        }else {
+            cachePath = context.getCacheDir().getPath();
+        }
+
+        return new File(cachePath+File.separator+uniqueName);
+    }
+
+    private long getUsableSpace(File path){
+        return  path.getUsableSpace();
+    }
+
+
+    private static class LoaderResult {
+        public ImageView imageView;
+        public String url;
+        public Bitmap bitmap;
+
+        public LoaderResult(ImageView imageView, String url, Bitmap bitmap) {
+            this.imageView = imageView;
+            this.url = url;
+            this.bitmap = bitmap;
+        }
+    }
+
 }
